ffe1e558839d0b0e757946c9830ee588795ae3d4
srmacpherson <srmacpherson96@gmail.com>
2025-10-24 16:39:38 +0100

    assign port to a variable in listen.js

--- listen.js
+++ listen.js
@@ -1,9 +1,11 @@
 const app = require("./app.js");
 
-app.listen(9090, (err) => {
+const PORT = 9090;
+
+app.listen(PORT, (err) => {
     if (err) {
         console.log(err);
     } else {
-        console.log("Server is listening on port 9090...");
+        console.log(`Server is listening on port ${PORT}...`);
     }
 });
